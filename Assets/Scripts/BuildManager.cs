50eb2aae36e0b46c91325b829ddf482fbb2a08c9
Shanzong <songfq24@gmail.com>
2020-06-21 08:31:18 -0700

    build turret

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    //singleton
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turrentToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turrentToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turrentToBuild;
+    }
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    private Renderer render;
+    private Color startColor;
+    public Vector3 offset;
+    private GameObject turret;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+        startColor = render.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        render.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        render.material.color = startColor;
+    }
+    private void OnMouseDown()
+    {
+        if(turret!= null)
+        {
+            Debug.Log("Already has a turret");
+            return;
+        }
+
+        //Build a turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+    }
+}
+
